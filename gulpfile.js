68bfd3ef3e4e7cbbf46a4e5c87955ccaadebfe88
Edmond-Wu <edmond.wu0895@gmail.com>
2018-05-13 09:58:01 -0400

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var cleanCss = require('gulp-clean-css');
+var minify = require('gulp-minify');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var series = require('gulp-series');
+
+/* packs vendor js files into a single file and minifies it */
+gulp.task('pack-js', function() {
+  return gulp.src(['node_modules/html5-boilerplate/dist/js/main.js', 'node_modules/angular/angular.min.js',
+    'node_modules/angular-route/angular-route.min.js', 'node_modules/angular-animate/angular-animate.min.js',
+    'node_modules/jquery/dist/jquery.min.js', 'node_modules/materialize-css/dist/js/materialize.min.js'])
+    .pipe(concat('vendor.js'))
+    .pipe(minify())
+    .pipe(gulp.dest('js'));
+});
+
+/* adds on app.js separately */
+gulp.task('add-app-js', function() {
+  return gulp.src(['js/vendor-min.js', 'js/app.js'])
+    .pipe(concat('build.js'))
+    .pipe(gulp.dest('js'));
+})
+
+/* packs css files into a single file and then minifies it */
+gulp.task('pack-css', function() {
+  return gulp.src(['node_modules/html5-boilerplate/dist/css/normalize.css', 'node_modules/html5-boilerplate/dist/css/main.css',
+    'node_modules/materialize-css/dist/css/materialize.min.css', 'node_modules/mdi/css/materialdesignicons.min.css', 'css/app.css'])
+    .pipe(concat('build.css'))
+    .pipe(cleanCss())
+    .pipe(gulp.dest('css'));
+});
+
+/* js stuff needs to be done sequentially */
+gulp.task(
+  'default',
+  gulp.parallel(
+    gulp.series('pack-js', 'add-app-js'),
+    'pack-css'
+  )
+);
